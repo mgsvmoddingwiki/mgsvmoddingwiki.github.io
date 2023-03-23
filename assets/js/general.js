72359cc77faea36a1949b7af23e240c13be798b2
chocmake <34178938+chocmake@users.noreply.github.com>
2023-03-23 22:35:15 +1100

    Improved File Formats page, added wiki width button
    - Converted File Formats page to Markdown tables, preserving original links, references and cell new lines.
    - Added wiki page width expansion button, so wider, dense tables can be better viewed on large monitors.

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,45 +1,57 @@
-window.onload = function() {
+// Add page width expansion toggle button
+var toolsButton = document.querySelector('.tools-element:nth-of-type(5)');
+var buttonExpand = document.createElement('span');
+buttonExpand.classList.add('tools-element','page-expand');
+buttonExpand.appendChild(document.createElement('span'));
+toolsButton.parentNode.insertBefore(buttonExpand, toolsButton.nextSibling); // insert after toolsButton
+// Add/remove class to body when clicked
+buttonExpand.addEventListener('click', function onClick(event) {
+    var body = document.body;
+    if (!body.classList.contains('expanded-width')) {
+        body.classList.add('expanded-width');
+    } else {
+        body.classList.remove('expanded-width');
+    }
+});
 
-    // Image handling
-    var img_list = document.querySelectorAll('.git-wiki-page img'); // returns NodeList
-    var img_array = [...img_list]; // converts NodeList to Array
-    img_array.forEach(img => {
+// Image handling
+var img_list = document.querySelectorAll('.git-wiki-page img'); // returns NodeList
+var img_array = [...img_list]; // converts NodeList to Array
+img_array.forEach(img => {
 
-        // Wrap images in links to image source
-        // Check if image already wrapped in link element (to not override it)
-        if (img.parentElement.tagName.toLowerCase() === 'a') {
-            var wrapperExists = true;
-            var wrapper = img.parentElement;
-        } else {
-            var wrapper = document.createElement('a');
-            wrapper.setAttribute('href', img.getAttribute('src'));
-        }
+    // Wrap images in links to image source
+    // Check if image already wrapped in link element (to not override it)
+    if (img.parentElement.tagName.toLowerCase() === 'a') {
+        var wrapperExists = true;
+        var wrapper = img.parentElement;
+    } else {
+        var wrapper = document.createElement('a');
+        wrapper.setAttribute('href', img.getAttribute('src'));
+    }
 
-        wrapper.classList.add(...img.classList); // copy img classes to link instead
-        wrapper.classList.add('image-wrapper');
-        img.removeAttribute('class');
-        if (!wrapperExists) {
-            wrapper.appendChild(img.cloneNode(true));
-            img.parentNode.replaceChild(wrapper, img);
-        }
+    wrapper.classList.add(...img.classList); // copy img classes to link instead
+    wrapper.classList.add('image-wrapper');
+    img.removeAttribute('class');
+    if (!wrapperExists) {
+        wrapper.appendChild(img.cloneNode(true));
+        img.parentNode.replaceChild(wrapper, img);
+    }
 
-        // Create caption element if image has alt text
-        if (img.getAttribute('alt')) {
-            var caption = document.createElement('small');
-            caption.classList.add('caption');
-            caption.textContent += img.getAttribute('alt');
-            wrapper.appendChild(caption);
-        }
+    // Create caption element if image has alt text
+    if (img.getAttribute('alt')) {
+        var caption = document.createElement('small');
+        caption.classList.add('caption');
+        caption.textContent += img.getAttribute('alt');
+        wrapper.appendChild(caption);
+    }
 
-    });
+});
 
-    // Video handling
-    var video_list = document.querySelectorAll('.git-wiki-page video');
-    var video_array = [...video_list];
-    video_array.forEach(video => {
+// Video handling
+var video_list = document.querySelectorAll('.git-wiki-page video');
+var video_array = [...video_list];
+video_array.forEach(video => {
 
-        video.volume = 0.5; // set default volume to 50%
+    video.volume = 0.5; // set default volume to 50%
 
-    });
-
-}
+});
